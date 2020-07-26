a53abbe9c193fbe9abed17eb607c5cb5f3186457
Akash Ambashankar <53367477+Akashamba@users.noreply.github.com>
2020-07-26 22:10:09 +0530

    Add files via upload

---
+++ static/js/navigation.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "60vw";
+  document.getElementById("mySidenav").style.boxShadow = "0px .5rem 1rem .5rem rgba(0,0,0,.2)";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.boxShadow = "0 .5rem 1rem .5rem rgba(0,0,0,0)";
+}
