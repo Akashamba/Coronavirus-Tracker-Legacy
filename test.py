d878c4cc5d477ad18a6bb7f3a03f3a3affaa8480
Akash Ambashankar <53367477+Akashamba@users.noreply.github.com>
2020-07-30 13:36:38 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
+from flask import Flask, render_template  # , request, redirect, url_for
 import requests
 import json
+import urllib3
+
+urllib3.disable_warnings()
 
 
 def comma(n):
@@ -8,22 +12,46 @@ def comma(n):
     return "".join([r] + d)
 
 
+def reorder(name):
+    try:
+        name.index(',')
+        return str(name.split(',', 1)[1]+" "+name.split(',', 1)[0])
+    except ValueError:
+        return name
+
+
 def get_url(url):
-    response = requests.get(url)
+    response = requests.get(url, verify=False)
     content = response.content.decode("utf8")
     return content
 
 
 def get_india_data():
-    total = json.loads(get_url("https://api.covidindiatracker.com/total.json"))
-    state = json.loads(get_url("https://api.covidindiatracker.com/state_data.json"))
-    india_data = [{"Confirmed": total["confirmed"], "Active": total["active"],
-                   "Recovered":total["recovered"], "Deaths": total["deaths"]}]
-    for i in range(len(state)):
-        india_data.append({"id": i + 1, "State": state[i]["state"], "Confirmed": comma(state[i]["confirmed"]),
-                           "Active": state[i]["active"], "Recovered": state[i]["recovered"], "Deaths": state[i]["deaths"]})
+    total = json.loads(get_url("https://api.covid19india.org/data.json"))
 
-    print(india_data)
+    india_data = [{"Confirmed": total["statewise"][0]["confirmed"],
+                   "Active": total["statewise"][0]["active"],
+                   "Recovered":total["statewise"][0]["recovered"],
+                   "Deaths": total["statewise"][0]["deaths"]}]
+    for i in range(1, len(total["statewise"])):
+        india_data.append({"id": i + 1, "State": total["statewise"][i]["state"],
+                           "Confirmed": int(total["statewise"][i]["confirmed"]),
+                           "Active": int(total["statewise"][i]["active"]),
+                           "Recovered": int(total["statewise"][i]["recovered"]),
+                           "Deaths": int(total["statewise"][i]["deaths"])})
+
+    india_data.sort(key=lambda x: (x['Deaths']), reverse=True)
+
+    for i in range(len(total["statewise"])):
+        if i != 0:
+            india_data[i]["id"] = i
+        india_data[i]["Confirmed"] = comma(india_data[i]["Confirmed"])
+        india_data[i]["Active"] = comma(india_data[i]["Confirmed"])
+        india_data[i]["Recovered"] = comma(india_data[i]["Confirmed"])
+        india_data[i]["Deaths"] = comma(india_data[i]["Confirmed"])
+
+    return india_data
 
 
-get_india_data()
+for i in range(37):
+    print(get_india_data()[i])
