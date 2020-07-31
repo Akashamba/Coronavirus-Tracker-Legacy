728000e9d1b3b90d517354f070d00e0797459e22
Akash Ambashankar <53367477+Akashamba@users.noreply.github.com>
2020-07-31 19:43:04 +0530

    Delete navigation.js

--- static/js/navigation.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-  document.getElementById("mySidenav").style.width = "60vw";
-  document.getElementById("mySidenav").style.boxShadow = "0 .5rem 1rem .5rem rgba(0,0,0,.2)";
-}
-
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("mySidenav").style.boxShadow = "0 .5rem 1rem .5rem rgba(0,0,0,0)";
-}
